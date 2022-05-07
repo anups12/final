d3080afc8b4de257f7378ac27f4a6d1614d7e9f8
Anup <kumar.anup459@gmail.com>
2022-05-08 02:57:33 +0530

    happy journey

--- final/settings.py
+++ final/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s-1sv0iuwm4^d&$zqmf7w%j$x1#1!)k6sypqv&)u1s$yhdqadb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['just4fun1.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
